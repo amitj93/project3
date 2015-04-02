e49d7aac889db0dd9b451fba27b0671f0fe3ac06
eabderh <eabderh@clemson.edu>
2015-04-02 19:34:52 -0400

    elias: initial update

--- allocator.c
+++ allocator.c
@@ -7,78 +7,89 @@
 
 #define PAGESIZE 4096
 
+
+
+
+
+
+
+
+
+
+struct PageArrayEntry {
+	struct PageSmallHeader* page;
+	unsigned int object_type;
+	}
+
+
+
+struct PageSmallHeader {
+	struct PageSmallHeader* prev;
+	struct PageSmallHeader* next;
+	//Segment* freesegment;
+	unsigned int object_count;
+	unsigned int object_max;
+	};
+
+struct PageLargeHeader {
+	struct PageLargeHeader* prev;
+	struct PageLargeHeader* next;
+	void* alloc_ptr;
+	unsigned int size_object;
+	unsigned int size_total;
+	};
+
+
+
+
+/*
 void __attribute__ ((constructor)) init(void);
 void __attribute__ ((destructor)) cleanup(void);
 
-static int init_allocator()
-{
+static int init_allocator() {
  fd = open("/dev/zero". 0_RDWR);
 
 for(i = 0; i< 255; i++)
    {
     page_t *startaddr[i] = NULL;
+    void *startaddr[i] = NULL;
+   }	}
 
-   }
-}
+*/
+
+
+
+
+
 
 void *malloc(size_t *size)
 {
+int segment_selection_size;
 
-if(size <= 2)
-{
-size = 2;
+PageArrayEntry page_array[PAGEARRAYCOUNT];
 
-}
-else if(size > 2 && size <=4)
-{
-size = 4;
 
-}
-else if(size > 4 && size <=8)
-{
-size = 8;
-}
-else if(size > 8 && size <=16)
-{
- size = 16;
-}
-else if(size > 16 && size <=32)
-{
- size = 32;
-}
+segment_selection_size = check_size(size);
+if (segment_selection_size != 0) {
+	page_small = page_array[segment_selection_size];
 
-else if(size > 32 && size <=64)
-{
- size = 64;
-}
 
-else if(size > 64 && size <=128)
-{
- size = 128;
-}
 
-else if(size > 128 && size <=256)
-{
- size = 256;
-}
 
-else if(size > 256 && size <=512)
-{
- size = 512;
-}
+	}
 
-else if(size > 512 && size <=1024)
-{
- size = 1024;
-}
-
-else if (size >= 1024)
-{
 
 }
 
-if(startaddr[i] == NULL)
-{
+
+
+
+
+
+
+
+
+if(startaddr[i] == NULL) {
 
 page_t * new = create_new_page(NULL, );
 if ( size < 1024)
@@ -91,16 +102,61 @@ if ( size < 1024)
 
 
 
-}
+	}
 
 else{
      mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
 
  
+	}
+
+
+
+
+
+
+
 }
 
+int check_size(int size)
+{
 
+if(size <= 2) {
+	size = 2;
+	}
+else if (size > 2 && size <=4) {
+	size = 4;
+	}
+else if (size > 4 && size <=8) {
+	size = 8;
+	}
+else if (size > 8 && size <=16) {
+	size = 16;
+	}
+else if (size > 16 && size <=32) {
+	size = 32;
+	}
+else if (size > 32 && size <=64) {
+	size = 64;
+	}
+else if (size > 64 && size <=128) {
+	size = 128;
+	}
+else if (size > 128 && size <=256) {
+	size = 256;
+	}
+else if (size > 256 && size <=512) {
+	size = 512;
+	}
+else if (size > 512 && size <=1024) {
+	size = 1024;
+	}
+else {
+	size = 0;
+	}
 
+return size;
+}
 
 }
 
@@ -112,7 +168,45 @@ void* my_calloc (size_t num, size_t size)
 
 
 void free (void* ptr) {
+/*
+int check_size(int size)
+{
 
+if(size <= 2) {
+	size = 2;
+	}
+else if (size > 2 && size <=4) {
+	size = 4;
+	}
+else if (size > 4 && size <=8) {
+	size = 8;
+	}
+else if (size > 8 && size <=16) {
+	size = 16;
+	}
+else if (size > 16 && size <=32) {
+	size = 32;
+	}
+else if (size > 32 && size <=64) {
+	size = 64;
+	}
+else if (size > 64 && size <=128) {
+	size = 128;
+	}
+else if (size > 128 && size <=256) {
+	size = 256;
+	}
+else if (size > 256 && size <=512) {
+	size = 512;
+	}
+else if (size > 512 && size <=1024) {
+	size = 1024;
+	}
+else {
+	size = 0;
+	}
+
+return size;
 }
 
 
@@ -137,4 +231,6 @@ ptr->num_blocks = 0;
 return pageptr;
 
 }
+*/
+
 
